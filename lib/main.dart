6ed16f70acd94d00581b74b5921fdbca8e610221
sllim637 <slim.hammami1977@gmail.com>
2022-06-12 14:17:25 +0100

    modification in appBar

--- lib/main.dart
+++ lib/main.dart
@@ -9,20 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
-          primarySwatch: Colors.deepOrange,
-        ),
-        home: soir());
+        debugShowCheckedModeBanner: false, title: 'Flutter Demo', home: soir());
   }
 }
 
@@ -33,21 +20,20 @@ class soir extends StatefulWidget {
 
 class _soirState extends State<soir> {
   String info = "bienvenue minsieur/madame";
-  bool val1 = false;
-  bool val2 = false;
-  double val3 = 0.0;
-  String value = "";
+  int value = 0;
 
-  void submit(String a){
-    setState((){
-      value = "message envoyé $a";
+  void increment() {
+    setState(() {
+      value++;
     });
   }
-  void afficher(String a){
-    setState((){
-      value = "bienvenue $a";
+
+  void decrement() {
+    setState(() {
+      value--;
     });
   }
+
   @override
   initState() {
     super.initState();
@@ -58,6 +44,29 @@ class _soirState extends State<soir> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
+        leading: const Icon(
+          Icons.fastfood,
+          size: 30,
+        ),
+        actions:  <Widget>[
+          IconButton(
+              onPressed: increment,
+              icon: Icon(
+                Icons.download,
+                size: 30,
+              )),
+          // Icon(Icons.download , size: 30,),
+          IconButton(
+              onPressed: decrement,
+              icon: Icon(
+                Icons.upload,
+                size: 30,
+              )),
+          Padding(padding: EdgeInsets.only(top: 40))
+          // Icon(Icons.upload , size: 30,)
+        ],
+        backgroundColor: Colors.red,
         title: Text(info),
       ),
       // ignore: deprecated_member_use
@@ -65,33 +74,11 @@ class _soirState extends State<soir> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text("Bienvenue  $value" , style: TextStyle(fontSize: 20 , color: Colors.orange), textAlign: TextAlign.center,),
-            TextField(
-              decoration: InputDecoration(
-                  labelText: "Nom",
-                  hintText: "entrer votre nom",
-                  icon: Icon(
-                    Icons.person,
-                    color: Colors.orange,
-                    size: 30,
-                  )),
-              keyboardType: TextInputType.text,
+            Text(
+              "the value is   $value",
+              style: TextStyle(fontSize: 20, color: Colors.orange),
+              textAlign: TextAlign.center,
             ),
-            TextField(
-              decoration: InputDecoration(
-                  labelText: "Telephone",
-                  hintText: "entrer votre numero de telephone",
-                  icon: Icon(
-                    Icons.phone,
-                    color: Colors.orange,
-                    size: 30,
-                  )),
-              keyboardType: TextInputType.number,
-              autocorrect: true,
-              autofocus: true,
-              onChanged: afficher ,
-               onSubmitted: submit ,
-            )
           ],
         ),
       ),
