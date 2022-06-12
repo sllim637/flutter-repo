a261310d7996f81e45a7c8e017b8b3496fad03d4
sllim637 <slim.hammami1977@gmail.com>
2022-06-12 13:19:55 +0100

    adding textField

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,31 +5,28 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.deepOrange,
-      ),
-      home: soir()
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.deepOrange,
+        ),
+        home: soir());
   }
-  
 }
-class soir extends StatefulWidget {
 
+class soir extends StatefulWidget {
   @override
   State<soir> createState() => _soirState();
 }
@@ -40,27 +35,25 @@ class _soirState extends State<soir> {
   String info = "bienvenue minsieur/madame";
   bool val1 = false;
   bool val2 = false;
-  double val3  = 0.0;
-  void onChangeVal1(bool s){
-    setState((){
-      val1=s;
-    });
-  }
-  void onChangeVal2(bool t){
-    setState((){
-      val2=t;
-    });
-  }
-  void onChangeVal3(double d){
-    setState((){
-      val3=d;
-    });
-  }
+  double val3 = 0.0;
+  String value = "";
 
-  initState(){
+  void submit(String a){
+    setState((){
+      value = "message envoyé $a";
+    });
+  }
+  void afficher(String a){
+    setState((){
+      value = "bienvenue $a";
+    });
+  }
+  @override
+  initState() {
     super.initState();
     info = "bienvenue sur le tuo";
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -70,18 +63,38 @@ class _soirState extends State<soir> {
       // ignore: deprecated_member_use
       body: Center(
         child: Column(
-          children:<Widget> [
-
-            SwitchListTile(value: val2, onChanged: onChangeVal2, activeColor: Colors.green ,
-              title: Text("valider interrupteur ")),
-            Text("valider le curseur ${(val3*100).round()}"),
-            Slider(value: val3, onChanged: onChangeVal3,)
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Bienvenue  $value" , style: TextStyle(fontSize: 20 , color: Colors.orange), textAlign: TextAlign.center,),
+            TextField(
+              decoration: InputDecoration(
+                  labelText: "Nom",
+                  hintText: "entrer votre nom",
+                  icon: Icon(
+                    Icons.person,
+                    color: Colors.orange,
+                    size: 30,
+                  )),
+              keyboardType: TextInputType.text,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                  labelText: "Telephone",
+                  hintText: "entrer votre numero de telephone",
+                  icon: Icon(
+                    Icons.phone,
+                    color: Colors.orange,
+                    size: 30,
+                  )),
+              keyboardType: TextInputType.number,
+              autocorrect: true,
+              autofocus: true,
+              onChanged: afficher ,
+               onSubmitted: submit ,
+            )
           ],
         ),
       ),
-    )
-    ;
+    );
   }
 }
-
-
